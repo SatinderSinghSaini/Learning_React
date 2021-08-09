3726a0824237d0e304415348cc0a423c146e50b8
Satinder Singh Saini <swv095@gmail.com>
2021-08-09 18:27:18 +0530

    Created First react app: Created Nested Component(Ninjas), passed data from parent to child

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Ninjas from './Ninjas';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App</h1>
+      <Ninjas name="Rahul" age="25" belt="Black"/>
+    </div>
+  );
+}
+
+export default App;
