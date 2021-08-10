bf1b766c07399d8737e53f2cf213793433aaabba
Satinder Singh Saini <swv095@gmail.com>
2021-08-10 18:19:54 +0530

    Passing data and events from child to parent and vice-versa. AddNinja component adds new ninja and delete ninja deletes existing ninja.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,39 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <Ninjas name="Rahul" age="25" belt="Black"/>
-    </div>
-  );
+class App extends Component {
+  state= {
+    ninjas:[
+      {name:"Rahul", age:"25", belt:"Black", id:1},
+      {name:"Rohan", age:"26", belt:"Blue", id:2},
+      {name:"Rishab", age:"27", belt:"Green", id:3}
+    ]
+  }
+
+  addNinja = (ninja)=>{
+    console.log(ninja);
+    let ninjas = [...this.state.ninjas,ninja];
+    ninja.id = Math.random();
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+  deleteNinja = (id) =>{
+    let ninjas = this.state.ninjas.filter(ninja=> ninja.id !== id);
+    this.setState({ninjas: ninjas});
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>My First React App</h1>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
+        <AddNinja addNinja={this.addNinja}/>
+      </div>
+    );
+  }  
 }
 
 export default App;
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-class Ninjas extends Component{
-    render() {
-        const {name, age, belt} = this.props;
+let Ninjas = (props)=>{
+        const {ninjas, deleteNinja} = props;
+        const ninjasList = ninjas.map(ninja=>{
+            return ninja.age>25 ?
+                    (
+                    <div className="ninja" key={ninja.id}>
+                        <div>Name: {ninja.name}</div>
+                        <div>Age: {ninja.age}</div>
+                        <div>Belt: {ninja.belt}</div>
+                        <button onClick={()=>{deleteNinja(ninja.id)}}>Delete Ninja</button>
+                    </div>
+                    ): null;
+        });
         return (
-            <div className="ninja">
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div>
+            <div className="ninja-list">
+                { ninjasList }
             </div>
         );
-    }
+    
 }
 
 export default Ninjas;
